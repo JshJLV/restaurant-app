7cccb042e08dee87de04aaee55ab7acdc1d36c1a
Joseph Lopez <joseph.lpz1999@gmail.com>
2023-03-21 20:01:30 -0600

    retirando svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="bg-neutral-700 w-full text-white flex flex-col justify-center items-center">
-        <div >
-          <h1>Siguenos en nuestras redes sociales</h1>
-          <ul className="flex">
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
-          </ul>
-        </div>
-        <div>
-          <p>
-            Pagina realizada por <Link target={"_blank"} to={"https://github.com/JshJLV"}>Joseph</Link> para Ucamp</p>
-        </div>
+      <div className="pt-4">
+        <h1>Siguenos en nuestras redes sociales</h1>
+        <ul className="flex justify-center py-2 px-4 ">
+          <li className="py-2 px-4 ">
+            <Link to={"#"}>
+              <FaFacebook className="w-8 h-8 hover:text-orange-600" />
+            </Link>
+          </li>
+          <li className="py-2 px-4">
+            <Link to={"#"}>
+              <FaInstagram className="w-8 h-8 hover:text-orange-600" />
+            </Link>
+          </li>
+          <li className="py-2 px-4">
+            <Link to={"#"}>
+              <FaTwitter className="w-8 h-8 hover:text-orange-600" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>
+          Pagina realizada por
+          <Link target={"_blank"} to={"https://github.com/JshJLV"}>
+            <span className="hover:text-orange-600"> Jsh</span>
+          </Link>{" "}
+          para Ucamp
+        </p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
